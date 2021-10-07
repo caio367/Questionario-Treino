fab246704e90d38c3dafeb05ba3c86c9b341eaed
Caio Antunes <caio.antuns@gmail.com>
2021-10-07 17:59:21 -0300

    Questionario
    Questionario

---
+++ treinando/src/main/java/com/questionario/treinando/Questionario.java
@@ -0,0 +1,79 @@
+package com.questionario.treinando;
+
+public class Questionario {
+
+    private String pergunta, altA, altB, altC, altD, altCorreta;
+    private int Nquest;
+
+
+    public String getPergunta() { return pergunta;}
+
+    public void setPergunta(String pergunta) { this.pergunta = pergunta;}
+
+    public String getAltA() { return altA; }
+
+    public void setAltA(String altA) { this.altA = altA; }
+
+    public String getAltB() { return altB;}
+
+    public void setAltB(String altB) { this.altB = altB; }
+
+    public String getAltC() { return altC; }
+
+    public void setAltC(String altC) { this.altC = altC; }
+
+    public String getAltD() { return altD; }
+
+    public void setAltD(String altD) { this.altD = altD; }
+
+    public String getAltCorreta() { return altCorreta; }
+
+    public void setAltCorreta(String altCorreta) { this.altCorreta = altCorreta; }
+
+    public int getNquest() { return Nquest; }
+
+    public void setNquest(int nquest) { Nquest = nquest; }
+
+    public Questionario() {
+    }
+
+    public Questionario(String pergunta, String altA, String altB, String altC, String altD, String altCorreta,
+            int nquest) {
+        this.pergunta = pergunta;
+        this.altA = altA;
+        this.altB = altB;
+        this.altC = altC;
+        this.altD = altD;
+        this.altCorreta = altCorreta;
+        Nquest = nquest;
+    }
+
+
+    
+
+    public void Escreva() {
+        System.out.println("-----------------------");
+        System.out.println("QUESTÃO " + Nquest);
+        System.out.println(pergunta);
+        System.out.println("-----------------------");
+        System.out.printf("( Opção A %s \n)", altA);
+        System.out.printf("( Opção B %s \n)", altB);
+        System.out.printf("( Opção C %s \n)", altC);
+        System.out.printf("( Opção D %s \n)", altD);
+        System.out.printf("Sua resposta: \n" );
+    }
+
+    public boolean VerificarResp(String respUsuario) {
+        return respUsuario == altCorreta;
+    }
+
+
+
+    
+
+
+
+
+
+    
+}
---
+++ treinando/src/main/java/com/questionario/treinando/TreinandoApplication.java
@@ -0,0 +1,86 @@
+package com.questionario.treinando;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinandoApplication.class, args);
+
+		String start;
+		int questCorretas = 0;
+
+		List<Questionario> listaPerg = new ArrayList<Questionario>();
+
+		Questionario p1 = new Questionario();
+		p1.setPergunta("Qual é o tipo de atributo que recebe palavras? ");
+		p1.setAltA("Int");
+		p1.setAltB("Boolean");
+		p1.setAltC("String");
+		p1.setAltD("Double");
+		p1.setAltCorreta("C");
+		p1.setNquest(1);
+		listaPerg.add(p1);
+
+		Questionario p2 = new Questionario();
+		p2.setPergunta("Qual dessas linguagens é mais antiga? ");
+		p2.setAltA("Python");
+		p2.setAltB("Java");
+		p2.setAltC("C Sharp (C#)");
+		p2.setAltD("Java Script");
+		p2.setAltCorreta("A");
+		p2.setNquest(2);
+		listaPerg.add(p2);
+
+		Scanner entrada = new Scanner(System.in);
+
+		Questionario playerGenerico = new Questionario();
+
+		System.out.println("Bem vindo ao ambiente virtual de testes!");
+		System.out.println("---------------------------");
+		System.out.println("Temos questõs de programação...");
+		System.out.printf("Deseja responder as perguntas? \nDigite 'S' para SIM e 'N' para NÂO: ");
+		start = entrada.next();
+
+		
+
+		Boolean resposta;
+		//String newListPerg = String.valueOf(listaPerg);
+	
+		int i = 0;
+
+		if(start == "S" || start == "s"); {
+			 for( i = 0; i<listaPerg.size(); i++) 
+				 listaPerg.get(i).Escreva();	
+				 resposta = playerGenerico.VerificarResp(entrada.next());
+				 
+				 if(resposta) {
+					 questCorretas++;
+				 } while (i < listaPerg);
+		}  
+		System.out.println("Respostas corretas: " + questCorretas); 
+			 
+			
+				//Questionario qst = new Questionario(listaPerg(i));
+			
+	
+
+		
+
+
+
+
+		entrada.close();
+	
+
+
+
+	}
+
+}
